bdfa926285d788b6dd1f9e5d0e336e4fabb86596
Jufarkas <139999007+Jufarkas@users.noreply.github.com>
2024-02-26 17:43:34 -0800

    make 2 variables global, rename variables for accuracy, add fnc to assign letter to game array based on btn click

--- script.js
+++ script.js
@@ -17,45 +17,73 @@
 //     newGame.increase();
 // })();
 
-(function(){
-    const body = document.querySelector('body');
-    const mainDoc = document.querySelector('.master-container');
-    const playerSelection = document.createElement('dialog');
-    let gridTracker = ["", "", "", "", "", "", "", "", "",];
-    let gameStart = {
+const mainDoc = document.querySelector('.master-container');
+const gridBtn = document.querySelectorAll('.game-button');
+
+(function() {
+    let gameInitialize = {
+        gridArray: ["", "", "", "", "", "", "", "", "",],
+        computerChoice: "",
         playerChoice: "",
         showGrid: function() {
             mainDoc.classList.remove('off');
         },
-        playerSelection: function() {
+        getPlayerSelection: function() {
+            const body = document.querySelector('body');
+            const selectionDialog = document.createElement('dialog');
             const xBtn = document.createElement('button');
             const oBtn = document.createElement('button');
             const introText = document.createElement('h1');
-            introText.textContent = "Select your champion:"
+            introText.textContent = "Select your champion:";
             xBtn.textContent = "X";
-            oBtn.textContent = "O"
-            body.appendChild(playerSelection);
-            playerSelection.showModal();
-            playerSelection.appendChild(introText);
-            playerSelection.appendChild(xBtn).classList.add('x-button');
-            playerSelection.appendChild(oBtn).classList.add('o-button');
-            playerSelection.addEventListener('click', (e) => {
+            oBtn.textContent = "O";
+            body.appendChild(selectionDialog);
+            selectionDialog.showModal();
+            selectionDialog.appendChild(introText);
+            selectionDialog.appendChild(xBtn).classList.add('x-button');
+            selectionDialog.appendChild(oBtn).classList.add('o-button');
+            selectionDialog.addEventListener('cancel', (e) => {
+                e.preventDefault();
+            });
+            selectionDialog.addEventListener('click', (e) => {
                 const target = e.target;
                 if(target === xBtn){
                     this.playerChoice = "X";
-                    playerSelection.close();
-                    console.log(this.playerChoice);
-                    gameStart.showGrid();
+                    this.computerChoice = "O";
+                    selectionDialog.close();
+                    console.log("player choice is: " + this.playerChoice); //remove once complete
+                    console.log("computer choice is: " + this.computerChoice); //remove once complete
+                    gameInitialize.showGrid();
                 } else if (target === oBtn){
                     this.playerChoice = "O";
-                    playerSelection.close();
-                    console.log(this.playerChoice);
-                    gameStart.showGrid();
+                    this.computerChoice = "X";
+                    selectionDialog.close();
+                    console.log("player choice is: " + this.playerChoice); //remove once complete
+                    console.log("computer choice is: " + this.computerChoice); //remove once complete
+                    gameInitialize.showGrid();
                 }
-            })
+            });
         },
     }
-    gameStart.playerSelection();
-    console.log(gridTracker.length);
-})();
+    gameInitialize.getPlayerSelection();
+    console.log(gameInitialize.gridArray.length);
 
+    let playGame = {
+        clickWatcher: function() {          
+            gridBtn.forEach(button => {
+                button.addEventListener('click', (e) => {
+                    let btnClasslist = e.target.classList;
+                    let btnPosition = btnClasslist.item(1);
+                    if(e.target.textContent === ""){
+                        e.target.textContent = gameInitialize.playerChoice;
+                        gameInitialize.gridArray[btnPosition] = gameInitialize.playerChoice;
+                        console.log(gameInitialize.gridArray);
+                    } else {
+                        return;
+                    }
+                })
+            })
+        }
+    }
+    playGame.clickWatcher();
+})();
