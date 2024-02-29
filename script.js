5d3383211686fc3257c9c2e804327bbb8513f1ef
Jufarkas <139999007+Jufarkas@users.noreply.github.com>
2024-02-29 12:06:51 -0800

    add timeouts so score updates before round resets, add new fnc roundOver and resetRound (both WIP), add check to stop comp turn from executing after player win

--- script.js
+++ script.js
@@ -45,9 +45,11 @@ const gridBtn = document.querySelectorAll('.game-button');
         },
     }
     gameInitialize.getPlayerSelection();
+    
     let winnerFound = false;
+    let roundEndCheck = 0;
     let playGame = {
-        clickWatcher: function() {
+        playersTurn: function() {
             let gameGrid = document.querySelector('.game-grid');
             gameGrid.addEventListener('click', (e) => { // listen to grid container, instead of individual buttons
                 if (e.target.matches('.game-button')) { 
@@ -58,13 +60,51 @@ const gridBtn = document.querySelectorAll('.game-button');
                         e.target.style.color = "rgb(0, 81, 255)";
                         e.target.textContent = gameInitialize.playerChoice;
                         gridArray[btnPosition] = gameInitialize.playerChoice;
-                        setTimeout(playGame.checkWinner, 20);
+                        roundEndCheck = 0;
+                        playGame.checkWinner();
                     } else {
                         return;
                     }
-                    setTimeout(playGame.computersTurn, 30);
+                    if (roundEndCheck === 1){ // once you create something to appear when the round is over (to start new round) if the player won make it run the 'computersTurn' function after it resets the grid, that way the computer goes first if the player wins, but by default the player will go first if the computer wins
+                        return;
+                    } else { 
+                        setTimeout(playGame.computersTurn, 30);
+                    }
                 }
             });
+        },
+
+        computersTurn: function() {
+            if (winnerFound === true) {
+                return;
+            } else if (gridArray == ["", "", "", "", "", "", "", "", "",]){
+                console.log(gridArray);
+                return;
+            } else if (gridArray.indexOf("") === -1){
+                gridArray = ["", "", "", "", "", "", "", "", "",];
+                gridBtn.forEach((button) => {
+                    button.textContent = "";
+                });
+                return;
+                // ADD THING TO DO WHEN THE GAME ENDS IN A TIE
+            };
+
+            let computerGenerator = Math.floor(Math.random() * 9);
+            let compGridBtn = document.querySelector('.game-grid button:nth-child('+ (computerGenerator +1) +')'); // gets the button for the grid from its class
+            if (gridArray[computerGenerator] != ""){
+                computerGenerator = Math.floor(Math.random() * 9);
+                setTimeout(playGame.computersTurn, 10);
+            } else if (gridArray[computerGenerator] === ""){
+                compGridBtn.style.color = "red";
+                compGridBtn.textContent = gameInitialize.computerChoice;
+                gridArray[computerGenerator] = gameInitialize.computerChoice;
+
+                playGame.checkWinner();
+                computerGenerator = Math.floor(Math.random() * 9);
+            } else {
+                return;
+            }
+            playGame.tieCheck();
         },
 
         tieCheck: function(){
@@ -76,35 +116,6 @@ const gridBtn = document.querySelectorAll('.game-button');
                 // ADD THING TO DO WHEN THE GAME ENDS IN A TIE
                 return;
             }
-        },
-
-        computersTurn: function() {
-            if (winnerFound === true) {
-                return;
-            }
-            let computerGenerator = Math.floor(Math.random() * 9);
-            let compGridBtn = document.querySelector('.game-grid button:nth-child('+ (computerGenerator +1) +')'); // gets the button for the grid from its class
-            if (gridArray.indexOf("") === -1){
-                gridArray = ["", "", "", "", "", "", "", "", "",];
-                gridBtn.forEach((button) => {
-                    button.textContent = "";
-                });
-                // ADD THING TO DO WHEN THE GAME ENDS IN A TIE
-                return;
-            } else if (gridArray[computerGenerator] != ""){
-                computerGenerator = Math.floor(Math.random() * 9);
-                setTimeout(playGame.computersTurn, 30);
-            } else if (gridArray[computerGenerator] === ""){
-                compGridBtn.style.color = "red";
-                compGridBtn.textContent = gameInitialize.computerChoice;
-                gridArray[computerGenerator] = gameInitialize.computerChoice;
-
-                setTimeout(playGame.checkWinner, 20);
-                computerGenerator = Math.floor(Math.random() * 9);
-            } else {
-                return;
-            }
-            setTimeout(playGame.tieCheck, 20);
         },
 
         checkWinner: function() {
@@ -127,57 +138,71 @@ const gridBtn = document.querySelectorAll('.game-button');
                 if (winningCombinations.some(subArray => subArray.every(values => gridArray[values] === player))) {
                     winnerFound = true;
                     winner = player;
+                    roundEndCheck = 1;
                     // ADD DIALOG SAYING WHO WON, AND TO CONTINUE
                 } else if (winningCombinations.some(subArray => subArray.every(values => gridArray[values] === computer))) {
                     winnerFound = true;
                     winner = computer;
                     // ADD DIALOG SAYING WHO WON, AND TO CONTINUE
-                }
+                };
                 playGame.increaseScore(winner);
-            }
+            };
         },
 
         increaseScore: function(winner) {
             if(winner === "X"){
                 gameInitialize.scoreX++;
                 document.querySelector('.x-score').textContent = gameInitialize.scoreX;
-                if(gameInitialize.scoreX === 5){
-                    if(gameInitialize.playerChoice === "X"){
-                        alert("player wins");
-                        // ADD BUTTON/DIALOG TO RESET GAME
+                setTimeout(() => {
+                    if(gameInitialize.scoreX === 5){
+                        if(gameInitialize.playerChoice === "X"){
+                            alert("player wins");
+                            // ADD BUTTON/DIALOG TO RESET GAME
+                        } else {
+                            alert("computer wins");
+                            // ADD BUTTON/DIALOG TO RESET GAME
+                        }
                     } else {
-                        alert("computer wins");
-                        // ADD BUTTON/DIALOG TO RESET GAME
+                        playGame.roundOver();
                     }
-                } else {
-                    gridArray = ["", "", "", "", "", "", "", "", "",];
-                    gridBtn.forEach((button) => {
-                        button.textContent = "";
-                    });
-                    winnerFound = false;
-                }
+                }, 20)
             } else if (winner === "O") {
                 gameInitialize.scoreO++;
                 document.querySelector('.o-score').textContent = gameInitialize.scoreO;
-                if(gameInitialize.scoreO === 5){
-                    if(gameInitialize.playerChoice === "O"){
-                        alert("player wins");
-                        // ADD BUTTON/DIALOG TO RESET GAME
+                setTimeout(() => {
+                    if(gameInitialize.scoreO === 5){
+                        if(gameInitialize.playerChoice === "O"){
+                            alert("player wins");
+                            // ADD BUTTON/DIALOG TO RESET GAME
+                        } else {
+                            alert("computer wins");
+                            // ADD BUTTON/DIALOG TO RESET GAME
+                        }
                     } else {
-                        alert("computer wins");
-                        // ADD BUTTON/DIALOG TO RESET GAME
+                        playGame.roundOver();
                     }
-                } else {
-                    gridArray = ["", "", "", "", "", "", "", "", "",];
-                    gridBtn.forEach((button) => {
-                        button.textContent = "";
-                    });
-                    winnerFound = false;
-                }
+                }, 20)
             }
-        }
+
+        },
+
+        roundOver: function() {
+            setTimeout(() => {
+                // alert("woohoo");
+                playGame.resetRound();
+            }, 20);
+        },
+
+        resetRound: function() {
+            gridArray = ["", "", "", "", "", "", "", "", "",];
+            gridBtn.forEach((button) => {
+                button.textContent = "";
+            });
+            winnerFound = false;
+        },
+        
     };
 
-    playGame.clickWatcher();
+    playGame.playersTurn();
     
 })();
